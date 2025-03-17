9fa9ef7521baccb795ca1d00262ce09e5db1f48d
laiszig <laisziegler@gmail.com>
2025-03-17 16:35:29 -0300

    packages and functions

--- functions/ReturnValues.go
+++ functions/ReturnValues.go
@@ -15,7 +15,7 @@ func main() {
 
 // Return values may be named - If so, they are treated as variables defined at the top of the function
 // These names should be used to document the meaning of the return values
-// A return statement without arguments returns the named return values. This is known as a "naked" return.
+// A return statement without arguments returns the named return values. This is known as a "naked" return
 // Naked return statements should be used only in short functions, as with the example shown here. They can harm readability in longer functions.
 func split(sum int) (x, y int) {
 	x = sum * 4 / 9
